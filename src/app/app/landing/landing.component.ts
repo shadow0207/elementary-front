380293bf6fb2072e8de561e5999c4a5084d63425
Sarbajit Biswas <sarbajit.biswas@wipro.com>
2018-04-15 18:31:55 +0530

    heroku connect

--- src/app/app/landing/landing.component.ts
+++ src/app/app/landing/landing.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
   }
-
+  redir() {
+    this.router.navigate(['/dashboard']);
+  }
 }
