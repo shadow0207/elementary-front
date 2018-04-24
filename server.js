7115d8b49bc1b400f4becd9db58119421c580d78
Sarbajit Biswas <sarbajit.biswas@wipro.com>
2018-04-24 11:31:30 +0530

    heroku

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
-
+app.get("/dashboard", function(req, res) {
+    console.log("DASHBOARD")
+});
 app.get('*', function(req, res) {
     console.log("/dashboard")
   res.sendfile('./server/views/index.html')
