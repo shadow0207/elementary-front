ccbf788fe8afd4c1596e15862ced69408e72dd83
Sarbajit Biswas <sarbajit.biswas@wipro.com>
2018-04-24 10:18:49 +0530

    heroku

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
-var path = require('path');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 
-app.get('/[^\.]+$', function (req, res) {
-    res.set('Content-Tpe', 'text/html').res.sendfile(path.join(__dirname, '/public/index.html'))
-});
+app.get('/[^\.]+$', function(req, res) {
+  res.sendfile('./server/views/index.html')
+})
